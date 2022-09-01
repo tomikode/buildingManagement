afe4030094506d936aec9089b65cdaf661fafcb3
tomikode <tomi.piisila@gmail.com>
2022-09-01 21:10:22 +1000

    basic database connection

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,8 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connect from "../../database/connection"
 
 export default function handler(req, res) {
+
+  connect().catch(err => console.log(error))
+
   res.status(200).json({ name: 'John Doe' })
 }
