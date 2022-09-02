88bb8090bc62a9de6e907bb8da4d058819734bea
tomikode <tomi.piisila@gmail.com>
2022-09-02 19:39:29 +1000

    simple prototype interface

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>
-    Building Management App
-  </div>;
+	return (
+		<div className={styles.container}>
+			<div>
+				<h1>Building Management App</h1>
+				<p>Prototype interface</p>
+			</div>
+		</div>
+	);
 }
