170618fb046c25fcd87861319e9dbe5dc51625db
tomikode <tomi.piisila@gmail.com>
2022-09-06 15:44:39 +1000

    small changes

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import mongoose from "mongoose"
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+
+  await mongoose.connect(process.env.MONGODB_URI)
+  const noteSchema = mongoose.Schema({
+    name: String
+  })
+
+  const Note = mongoose.model('Note', noteSchema)
+
+  const newNote = new Note({ name: 'test' })
+
+  await newNote.save()
+
+  res.status(200).json({ name: 'test' })
 }
