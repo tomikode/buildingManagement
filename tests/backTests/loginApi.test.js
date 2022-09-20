bd0b0b3886f38d4f3b8c3e149b148eb6d789105c
tomikode <tomi.piisila@gmail.com>
2022-09-20 18:51:41 +1000

    changed to jest only, launching dev from js

--- tests/backTests/loginApi.test.js
+++ tests/backTests/loginApi.test.js
@@ -1,11 +1,33 @@
 import axios from "axios";
+import { exec } from "node:child_process";
+
+jest.retryTimes(1000)
+
+let testServer;
+
+beforeAll(() => {
+	console.log("spawning")
+	testServer = exec(
+		/^win/.test(process.platform) ? "npm.cmd" : "npm",
+		["run", "dev"]
+	);
+});
+
+afterAll(() => {
+	console.log("killing")
+	testServer.kill()
+})
 
 describe("apitest", () => {
 	it("might work", async () => {
+		
 		try {
 			const res = await axios.get("http://localhost:3000/api/login");
 		} catch (e) {
-			expect(e.response.status).toBe(401);
+			// if (e.response)
+				expect(e.response.status).toBe(401);
+			// else
+				// console.log("something else went wrong")
 		}
 	});
 });
