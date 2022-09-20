9452e4cd30af255ae7ab496aa5c3b1f6a7bfd2ae
tomikode <tomi.piisila@gmail.com>
2022-09-20 19:14:12 +1000

    checking version, adding more retries

--- tests/backTests/loginApi.test.js
+++ tests/backTests/loginApi.test.js
@@ -1,33 +1,29 @@
 import axios from "axios";
 import { exec } from "node:child_process";
 
-jest.retryTimes(1000)
+jest.retryTimes(10000);
 
 let testServer;
 
 beforeAll(() => {
-	console.log("spawning")
-	testServer = exec(
-		/^win/.test(process.platform) ? "npm.cmd" : "npm",
-		["run", "dev"]
-	);
+	console.log("spawning");
+	testServer = exec(/^win/.test(process.platform) ? "npm.cmd" : "npm", [
+		"run",
+		"dev",
+	]);
 });
 
 afterAll(() => {
-	console.log("killing")
-	testServer.kill()
-})
+	console.log("killing");
+	testServer.kill();
+});
 
 describe("apitest", () => {
 	it("might work", async () => {
-		
 		try {
 			const res = await axios.get("http://localhost:3000/api/login");
 		} catch (e) {
-			// if (e.response)
-				expect(e.response.status).toBe(401);
-			// else
-				// console.log("something else went wrong")
+			expect(e.response.status).toBe(401);
 		}
 	});
 });
