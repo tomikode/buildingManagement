c3501163f7b4abd8d1bc2f9d65fb31c069fd98ec
lrj <12937177@student.uts.edu.au>
2022-09-22 15:52:45 +1000

    Add pages/userManagement.js
    Not too sure if other pages are required in structure, but copied
    contractorHome.js and changed the relevant words. Tested working
    OK.

---
+++ pages/userManagement.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const UserManagement = () => {
+	return <Layout pageType="c">User Management</Layout>;
+};
+
+export default UserManagement;
