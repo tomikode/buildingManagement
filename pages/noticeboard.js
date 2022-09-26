4346a669b049ada1ffad2a7817be22d6b8a02415
lrj <12937177@student.uts.edu.au>
2022-09-26 14:48:32 +1000

    Noticeboard now pulls live data from MongoDB
    Although there aren't any notices yet the system now (I think)
    pulls data from the live database and presents it to the user.
    Will develop CRUD operations here on notices leaving user
    management to last due to the risks involved.

--- pages/noticeboard.js
+++ pages/noticeboard.js
@@ -1,16 +1,13 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import Layout from "../components/Layout";
 import styles from "../styles/Profile.module.css";
 import NoticeList from "../components/noticeboard/NoticeList";
 import Button from "../components/Button";
 import EditNotice from "../components/noticeboard/EditNotice";
-import { useRouter } from "next/router";
 import axios from "axios";
 
 const Noticeboard = () => {
-  //const router = useRouter();
   const NoticeContext = createContext();
-  //const noticeCon = useContext(NoticeContext);
 
   // Constants
   const NO_SELECTED_NOTICE = 0;
@@ -20,13 +17,33 @@ const Noticeboard = () => {
     EDIT_NOTICE: 2,
     CREATE_NOTICE: 3,
   };
+  const [notices, setNotices] = useState([]);
+
+  useEffect(() => {
+    const getNotices = async () => {
+      const noticesFromMongo = await fetchNotices();
+      setNotices(noticesFromMongo);
+    };
+    getNotices();
+  }, []);
+
+  const fetchNotices = async () => {
+    try {
+      const res = await axios.post("/api/noticeboard", null);
+      var loadedNotices = JSON.parse(JSON.stringify(res.data.foundNotices));
+      return loadedNotices;
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
 
   // State
   const [activeViewState, setActiveViewState] = useState(
     ACTIVE_VIEW.NOTICEBOARD_LIST
   );
   const [selectedNoticeID, setSelectedNoticeID] = useState(NO_SELECTED_NOTICE);
-  const [notices, setNotices] = useState([
+  //const [notices, setNotices] = useState([
+  [
     {
       id: 1,
       userID: 1,
@@ -46,7 +63,7 @@ const Noticeboard = () => {
       postedAt: "Now",
       content: "I have to stop hitting Ctrl-Shft-S.",
     },
-  ]);
+  ];
 
   // Methods, are they even methods?
   const getNotice = (id) => {
@@ -59,12 +76,6 @@ const Noticeboard = () => {
       const noticeID = {
         id: "1",
       };
-      const res = await axios.post("/api/noticeboard", noticeID);
-<<<<<<< HEAD
-      console.log("Here's the user:", JSON.stringify(res.data.foundUsers));
-=======
-      console.log("Here's the user:", JSON.stringify(res.data.foundUser));
->>>>>>> 5cee7ad (Implement simple database lookup for Noticeboard)
     } catch (e) {
       console.log(e.message);
     }
