ea0005f1356a343842207d7865a04eeb153bb532
tomikode <tomi.piisila@gmail.com>
2022-09-29 17:01:19 +1000

    fixed backend tests, added login backend tests, added mongodb test env

--- tests/backTests/loginApi.test.js
+++ tests/backTests/loginApi.test.js
@@ -1,35 +1,36 @@
 import axios from "axios";
-import { spawn } from "node:child_process";
 
-jest.retryTimes(10000);
+let loginApi = "http://localhost:3000/api/login";
+const validCreds = {
+	email: "tom@mail",
+	password: "tom",
+};
 
-let testServer;
+export default function loginTest() {
+	describe("login api", () => {
+		it("get request", async () => {
+			try {
+				const res = await axios.get(loginApi);
+			} catch (e) {
+				expect(e.response.status).toBe(401);
+				expect(e.response.data.error).toBe("Something went wrong");
+				jest.retryTimes(1000);
+			}
+		});
+		it("post request with no credentials", async () => {
+			try {
+				const res = await axios.post(loginApi, {});
+			} catch (e) {
+				expect(e.response.status).toBe(401);
+				expect(e.response.data.error).toBe("Invalid credentials");
+			}
+		});
 
-beforeAll(() => {
-	console.log("spawning");
-	testServer = spawn(/^win/.test(process.platform) ? "npm.cmd" : "npm", [
-		"run",
-		"dev",
-	]);
-	testServer.stdout.on("data", (data) => {
-		console.log(`stdout: ${data}`);
+		it("post request with correct credentials", async () => {
+			const res = await axios.post(loginApi, validCreds);
+			console.log(res);
+			expect(res.status).toBe(201);
+			expect(res.data.foundUser.email).toBe(validCreds.email);
+		});
 	});
-
-	testServer.stderr.on("data", (data) => {
-		console.error(`stderr: ${data}`);
-	});
-
-	testServer.on("close", (code) => {
-		console.log(`child process exited with code ${code}`);
-	});
-});
-
-describe("apitest", () => {
-	it("might work", async () => {
-		try {
-			const res = await axios.get("http://localhost:3000/api/login");
-		} catch (e) {
-			expect(e.response.status).toBe(401);
-		}
-	});
-});
+}
