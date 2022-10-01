d26ec130d1684027de9d9bb94cacfba91a75ac34
Michael Ning <crown.d@hotmail.com.au>
2022-10-01 12:54:57 +1000

    First commit to Rental Pages

---
+++ pages/rentalPayment.js
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout";
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+	return (
+		<Layout>
+			<div className={styles.container}>
+				<div>
+					<h1>Rental Payment</h1>
+					<p>Rental Payment Page</p>
+				</div>
+			</div>
+		</Layout>
+	);
+}
