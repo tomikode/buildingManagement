e37f4d524cba05aad0467a38a65d41fe57ce7653
tomikode <tomi.piisila@gmail.com>
2022-10-01 11:37:22 +1000

    fixed mistake, check pipeline

--- components/TenantNav.js
+++ components/TenantNav.js
@@ -33,7 +33,7 @@ const TenantNav = () => {
 								<p>Mail</p>
 							</li>
 						</Link>
-                        <Link href="/noticeboard">
+						<Link href="/noticeboard">
 							<li>
 								<p>Noticeboard</p>
 							</li>
@@ -53,16 +53,12 @@ const TenantNav = () => {
 								<p>Invoices</p>
 							</li>
 						</Link>
-<<<<<<< HEAD
-						<Link href="/logout">
-=======
 						<Link href="/userManagement">
 							<li>
 								<p>User Management</p>
 							</li>
 						</Link>
-                        <Link href="/logout">
->>>>>>> 67ecbe725481bfd34edda1dd88182aac9c197aaa
+						<Link href="/logout">
 							<li>
 								<p>Logout</p>
 							</li>
