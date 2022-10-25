d74127d47d44b1d98efb890d17f28ab39e51828a
SalmanChandio <112498719+SalmanChandio@users.noreply.github.com>
2022-10-25 12:55:24 +1100

    Added Comments

--- pages/Invoice/contractorInvoicev2.js
+++ pages/Invoice/contractorInvoicev2.js
@@ -7,16 +7,16 @@ import ViewInvoice from "../../components/invoice/ViewInvoice";
 import styles from "../../styles/Rental.module.css";
 
 const contractorInvoice = () => {
-    const userCont = useContext(UserContext);
-    const contractInvoices = useRef([]);
-    const [showCreate, setShowCreate] = useState(false);
-    const [filterInvoice, setFilterInvoice] = useState(contractInvoices.current);
+    const userCont = useContext(UserContext);       //Recieve user
+    const contractInvoices = useRef([]);            //setup invoices
+    const [showCreate, setShowCreate] = useState(false);   
+    const [filterInvoice, setFilterInvoice] = useState(contractInvoices.current);  //Filters invoices to current user
     const [showView, setShowView] = useState(null);
 
     const fetchInvoices = async () => {
-        const response = await axios.get("api/Invoicev2");
+        const response = await axios.get("api/Invoicev2"); 
         contractInvoices.current = response.data.filter(
-            (invoice) => invoice.user._id === userCont.user._id
+            (invoice) => invoice.user._id === userCont.user._id //Finds invoice by user id
         );
         setFilterInvoice(contractInvoices.current);
     };
@@ -25,7 +25,7 @@ const contractorInvoice = () => {
         if (userCont.user) fetchInvoices();
     }, [userCont]);
 
-    const openView = (invoice) => {
+    const openView = (invoice) => {     //Initialises opening invoices
         console.log("thing");
         setShowView(invoice);
     }
@@ -36,7 +36,7 @@ const contractorInvoice = () => {
 
     const createInvoice = async (invoice) => {
         contractInvoices.current.push(invoice);
-        const format = {
+        const format = {                     //Initialising code to create invoice
             user: invoice.user,
             job: invoice.job,
             amount: invoice.amount,
@@ -44,7 +44,7 @@ const contractorInvoice = () => {
             description: invoice.description,
         };
 
-        await axios.post("api/Invoicev2", format);
+        await axios.post("api/Invoicev2", format);  
         fetchInvoices();
         closeCreate();
     }
@@ -58,7 +58,7 @@ const contractorInvoice = () => {
     };
 
     const updateInvoice = async (invoice) => {
-        const res = await axios.put(`api/Invoicev2/${invoice._id}`, invoice);
+        const res = await axios.put(`api/Invoicev2/${invoice._id}`, invoice);   //Used to obtain details of invoice to update
         console.log(res);
         fetchWorkOrders();
         closeView();
@@ -85,11 +85,11 @@ const contractorInvoice = () => {
                 <div className={styles.box}>
                     <h1>Invoices</h1>
                     <div className={styles.buttons}>
-                        <button classname={styles.buttons} onClick={openCreate}> Create </button>
+                        <button classname={styles.buttons} onClick={openCreate}> Create </button>   //Used to open create functionality
                     </div>
                 </div>
                 <div className={styles.tableContainer}>
-                    <table className={styles.table}>
+                    <table className={styles.table}>        //Creates table to display data
                         <thead className={styles.thead}>
                             <tr>
                                 <td>ID</td>
@@ -101,7 +101,7 @@ const contractorInvoice = () => {
                         </thead>
                         <tbody>
                             {filterInvoice.map((invoice, index) => (
-                                <tr key = {index}> //onClick={() => openView(invoice)}>
+                                <tr key = {index} onClick={() => openView(invoice)}>
                                     <td>{invoice.user.firstName}</td>
                                     <td>{invoice.job}</td>
                                     <td>{invoice.amount}</td>
